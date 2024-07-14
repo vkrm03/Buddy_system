c25cc2842396f2990bad46f8e5e53e73ba1228f1
vikram <vikramrokith03@gmail.com>
2024-07-14 12:05:43 +0530

    Admin Staff Student page upgraded.

--- index.js
+++ index.js
@@ -16,7 +16,30 @@ db.connect();
 const app = express();
 const port = 3000;
 
-let currnt_Id, currnt_image_url ,currnt_role;
+let currnt_Id, currnt_image_url ,currnt_role,currentStudentId, Admin;
+let currentStudentImageUrl;
+
+const students = [
+    {
+        id: 1,
+        name: "arun",
+        email: "arun@example.com",
+        password: "password1",
+        gender: "Male",
+        mobile_no: "1234567890",
+        image_url: "/student.jpg"
+    },
+    {
+        id: 2,
+        name: "safiya",
+        email: "safiya@example.com",
+        password: "password2",
+        gender: "Female",
+        mobile_no: "0987654321",
+        image_url: "/student.jpg"
+    }
+];
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -24,7 +47,11 @@ app.use(express.static("public"));
 function checkAuth(req, res, next) {
     if (currnt_Id) {
         next();
-    } else {
+    } else if (currentStudentId) {
+        next();
+    } else if (Admin) {
+        next();
+    }else {
         res.redirect("/login?error=Please log in first");
     }
 }
@@ -38,7 +65,7 @@ app.get("/about", async (req, res) => {
     res.render("about.ejs");
 });
 
-app.get("/student-mentees", async (req, res) => {
+app.get("/mentees", async (req, res) => {
     const result = await db.query('SELECT * FROM mentor_student_information');
     data = result.rows
     res.render("student-mentees-15.ejs", {datas:data});
@@ -64,37 +91,55 @@ app.post("/login", async (req, res) => {
     const email = req.body.email
     const pass = req.body.pass
     const usr_type = req.body.roles
-    currnt_role = usr_type
+    currnt_role = usr_type;
+    console.log(currnt_role);
     try {
-        const result = await db.query('SELECT * FROM staff_information WHERE email = $1', [email]);
-        if (result.rows.length > 0) {
-            const user = result.rows[0];
-            currnt_Id = user.id
-            if (currnt_Id == 1) {
-                currnt_image_url = "/A1_staff.jpg"
-            } else if (currnt_Id == 2) {
-                currnt_image_url = "/A2_staff.jpg"
-            }
-            else if (currnt_Id == 3) {
-                currnt_image_url = "/A3_staff.jpg"
-            }
-            else if (currnt_Id == 4) {
-                currnt_image_url = "/A4_staff.jpg"
-            }else  {
-                currnt_image_url = "/A5_staff.jpg"
-            }
-            if (user.passwords == pass) {
-                res.redirect("/dashboard?userId=" + currnt_Id + "&success=true");
+        if (currnt_role === "adm") {
+            if (email === "Admin@admin.com" && pass === "Password") {
+                Admin = true;
+                res.redirect("/admin-dashboard" + "?success=true");
             } else {
                 res.redirect("/login?error=Invalid password");
             }
-        } else {
-            res.redirect("/login?error=User Not Found");
+        } else if (currnt_role === "stf") {
+            try {
+                const result = await db.query('SELECT * FROM staff_information WHERE email = $1', [email]);
+                if (result.rows.length > 0) {
+                    const user = result.rows[0];
+                    currnt_Id = user.id
+                    if (currnt_Id == 1) {
+                        currnt_image_url = "/A1_staff.jpg"
+                    } else if (currnt_Id == 2) {
+                        currnt_image_url = "/A2_staff.jpg"
+                    }
+                    else if (currnt_Id == 3) {
+                        currnt_image_url = "/A3_staff.jpg"
+                    }
+                    else if (currnt_Id == 4) {
+                        currnt_image_url = "/A4_staff.jpg"
+                    }else  {
+                        currnt_image_url = "/A5_staff.jpg"
+                    }
+                    if (user.passwords == pass) {
+                        res.redirect("/dashboard?userId=" + currnt_Id + "&success=true");
+                    } else {
+                        res.redirect("/login?error=Invalid password");
+                    }
+                } else {
+                    res.redirect("/login?error=User Not Found");
+                }
+            } catch (err) {
+                console.error(err);
+                res.redirect("/login?error=An error occurred");
+            }
+        } else if (currnt_role === "std") {
+            currentStudentId = 1;
+            res.redirect("/student-dashboard?userId=" + currentStudentId + "&success=true");
         }
-    } catch (err) {
-        console.error(err);
-        res.redirect("/login?error=An error occurred");
-    }
+    
+} catch (err) {
+    console.log(err);
+}
 });
 
 
@@ -118,10 +163,46 @@ app.get("/dashboard",checkAuth, async (req, res) => {
     }
 });
 
-app.get("/odop",checkAuth, async (req, res) => {
-    const staff = await db.query('SELECT * FROM staff_information WHERE id = $1', [currnt_Id]);
-    staff_name = staff.rows[0].name
-    res.render("odop.ejs", {StaffName:staff_name, image_url: currnt_image_url});
+
+
+
+app.get("/student-dashboard",checkAuth, async (req, res) => {
+    const student = students.find(stu => stu.id === 1);
+    const success = req.query.success;
+    res.render("student_dashboard.ejs", {
+        StudentName: student.name,
+        StudentEmail: student.email,
+        StudentGender: student.gender,
+        StudentMobileNum: student.mobile_no,
+        success: success
+    });
+});
+
+app.get("/admin-dashboard", async (req, res) => {
+    const success = req.query.success;
+    res.render("admin-dashboard.ejs", {success: success})
+});
+
+
+app.get("/odop-std",checkAuth, async (req, res) => {
+    res.render("odop-std.ejs");
+});
+
+app.get("/edit-admin",checkAuth, async (req, res) => {
+    res.render("edit-admin.ejs");
+});
+
+app.get("/edit-student-datas",checkAuth, async (req, res) => {
+    res.render("edit-student.ejs");
+});
+
+app.get("/my-stats",checkAuth, async (req, res) => {
+    res.render("my-stats.ejs");
+});
+
+
+app.get("/odop-question",checkAuth, async (req, res) => {
+    res.render("odop-question.ejs");
 });
 
 app.get("/edit-student",checkAuth, async (req, res) => {
